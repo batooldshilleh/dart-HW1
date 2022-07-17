908b1ca2883df6eec34c30a86b1384969e56d179
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2022-07-17 11:28:42 +0300

    Create hw1.dart

---
+++ hw1.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = "Batool";
+  bool Marital_status = false;
+  int old = 20;
+  double gpa = 3.0;
+  print("name: ${name}");
+  print("age:" + old.toString());
+  print("married:" + Marital_status.toString());
+  print("gpa:${gpa}");
+}
